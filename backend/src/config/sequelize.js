c1c35eae3d6efab094c0e3e0b4930b87276a7746
UjjawalVerma194 <vermaujjawal77@gmail.com>
2026-06-05 12:08:36 +0530

    updated sequelite file

--- backend/src/config/sequelize.js
+++ backend/src/config/sequelize.js
@@ -4,6 +4,17 @@ const config = require('./index');
 const sequelize = new Sequelize(config.databaseUrl, {
   dialect: 'postgres',
   logging: config.nodeEnv === 'development' ? console.log : false,
+
+  dialectOptions:
+    config.nodeEnv === 'production'
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        }
+      : {},
+
   define: {
     underscored: true,
     timestamps: true,
